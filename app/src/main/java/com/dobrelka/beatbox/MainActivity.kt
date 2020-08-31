64db8d6ed33c7cab5822f076d5a9c19184810566
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-08-31 16:54:22 +0300

    Adding val binding in MainActivity.kt

--- app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
+++ app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
@@ -2,10 +2,15 @@ package com.dobrelka.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.dobrelka.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding: ActivityMainBinding =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
