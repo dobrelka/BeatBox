c9d1d7a8857accd2209c01fb1b73d34eccd300f8
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-08-31 17:05:59 +0300

    Adding binding.recyclerView in MainActivity.kt

--- app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
+++ app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dobrelka.beatbox
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
 import com.dobrelka.beatbox.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // Inflating a binding class
         val binding: ActivityMainBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Configuring RecyclerView
+        binding.recyclerView.apply {
+            layoutManager = GridLayoutManager(context, 3)
+        }
     }
 }
