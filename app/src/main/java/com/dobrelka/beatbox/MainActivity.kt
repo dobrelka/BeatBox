f9263e89bbaf737295dd066370b9ed54fa27ae55
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-09-03 15:26:02 +0300

    Adding private inner class SoundAdapter in MainActivity.kt

--- app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
+++ app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
@@ -2,6 +2,7 @@ package com.dobrelka.beatbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,5 +27,26 @@ class MainActivity : AppCompatActivity() {
     private inner class SoundHolder(private val binding: ListItemSoundBinding) :
             RecyclerView.ViewHolder(binding.root) {
 
+    }
+
+    private inner class SoundAdapter() :
+        RecyclerView.Adapter<SoundHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):
+                SoundHolder {
+            val binding = DataBindingUtil.inflate<ListItemSoundBinding>(
+                layoutInflater,
+                R.layout.list_item_sound,
+                parent,
+                false
+            )
+            return SoundHolder(binding)
+        }
+
+        override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+
+        }
+
+        override fun getItemCount() = 0
     }
 }
