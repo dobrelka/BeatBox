c91f47685f7250732ad0b5efc0629355c33ff2de
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-09-04 17:53:25 +0300

    Adding fun loadSounds in BeatBox.kt

--- app/src/main/java/com/dobrelka/beatbox/BeatBox.kt
+++ app/src/main/java/com/dobrelka/beatbox/BeatBox.kt
@@ -1,10 +1,25 @@
 package com.dobrelka.beatbox
 
 import android.content.res.AssetManager
+import java.lang.Exception
+import android.util.Log
 
 private const val TAG = "BeatBox"
 private const val SOUNDS_FOLDER = "sample_sounds"
 
 // Stashing an AssetManager for safekeeping
 class BeatBox(private val assets: AssetManager) {
+
+    // Listing of what you have in your assets, you can  use the list(String) function.
+    //Function called loadSounds()  looks in your assets with list(String)
+    fun loadSounds(): List<String> {
+        try {
+            val soundNames = assets.list(SOUNDS_FOLDER)!!
+            Log.d(TAG, "Found ${soundNames.size} sounds")
+            return soundNames.asList()
+        } catch (e: Exception) {
+            Log.e(TAG, "Could not list assets", e)
+            return emptyList()
+        }
+    }
 }
