4f31333ba64c32c07b4f2aad1cb1ecea33ead11a
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-09-09 12:26:36 +0300

    Adding beatBox.sounds in fun onCreate into MainActivity.kt

--- app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
+++ app/src/main/java/com/dobrelka/beatbox/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // Configuring RecyclerView
         binding.recyclerView.apply {
             layoutManager = GridLayoutManager(context, 3)
-            adapter = SoundAdapter()
+            adapter = SoundAdapter(beatBox.sounds)
         }
     }
 
