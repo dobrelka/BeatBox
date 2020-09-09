33d775e0c3d1b74c1784b853ddf3cd78a08024ad
LIUDMYLA <66875326+dobrelka@users.noreply.github.com>
2020-09-09 12:14:09 +0300

    Adding val soundNames, val sounds in BeatBox.kt

--- app/src/main/java/com/dobrelka/beatbox/BeatBox.kt
+++ app/src/main/java/com/dobrelka/beatbox/BeatBox.kt
@@ -10,16 +10,32 @@ private const val SOUNDS_FOLDER = "sample_sounds"
 // Stashing an AssetManager for safekeeping
 class BeatBox(private val assets: AssetManager) {
 
+    val sounds: List<Sound>
+
+    init {
+        sounds = loadSounds()
+    }
+
     // Listing of what you have in your assets, you can  use the list(String) function.
     //Function called loadSounds()  looks in your assets with list(String)
-    fun loadSounds(): List<String> {
+    fun loadSounds(): List<Sound> {
+
+        val soundNames: Array<String>
+
         try {
-            val soundNames = assets.list(SOUNDS_FOLDER)!!
-            Log.d(TAG, "Found ${soundNames.size} sounds")
-            return soundNames.asList()
+            soundNames = assets.list(SOUNDS_FOLDER)!!
+
         } catch (e: Exception) {
             Log.e(TAG, "Could not list assets", e)
             return emptyList()
         }
+        val sounds = mutableListOf<Sound>()
+        soundNames.forEach {filename ->
+            val assetPath = "$SOUNDS_FOLDER/$filename"
+            val sound = Sound(assetPath)
+            sounds.add(sound)
+
+        }
+        return sounds
     }
 }
